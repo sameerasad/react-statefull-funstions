88446f01375e3ab8548703bb19032d4d7d68f7d6
sameerasad <ssameerkhan525@gmail.com>
2020-07-19 08:01:55 -0400

    add css and and iplement it on some conditional bases

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, {useState}  from 'react'
 import {Message} from './Message'
+import './App.css';
 
      const App = () => {
    
     const [count, setCount] = useState(0)
+    let [isDay, setDay] = useState(true)
 
     return (
-        <div>
+        <div className={`box ${isDay ? "dayLight" : ""}`}>
             <Message counter={count} />
             <br/>
             <button onClick={()=>setCount(count+1)}  >Click Me</button>
+
+
+            
+            <br/>
+            <br/>
+
+            <button onClick={()=>setCount(count-1)}>Click Me For Decrement</button>
+            <br/>
+            <br/>
+            <button onClick={()=>setDay(!isDay)}>changeColor</button>
         </div>
     )
 }
